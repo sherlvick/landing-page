cb2dde94a6441e0f6f7f58874eced6dac8c768ca
sherlvick <kvicky047@gmail.com>
2020-09-14 18:11:46 +0530

    This is my first project on React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,107 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import pic1 from './download.jpg';
+import pic2 from './download1.jpg';
+import pic3 from './download2.jpg';
+import pdf from "./about.pdf";
 
+const Body= ()=>{
+  return(
+  <div id='body'>
+    <Header/>
+    <Card
+      className='section'
+      img= {pic1}
+      title='About the company'
+      description='We provide service online and offline both for our customers.
+                  Most of the troubleshooting that does not require experts presence
+                  are executed online.In case of helper or expert demands we fulfill
+                  it within a day. '/>
+    <Card
+      className='section bg-grey'
+      img={pic2}
+      title='Infrastructures and Alumni'
+      description='We provide service online and offline both for our customers.
+                  Most of the troubleshooting that does not require experts presence
+                  are executed online.In case of helper or expert demands we fulfill
+                  it within a day. '/>
+    <Card
+      className='section'
+      img={pic3}
+      title='Experts and Helpdesk'
+      description='We provide service online and offline both for our customers.
+                  Most of the troubleshooting that does not require experts presence
+                  are executed online.In case of helper or expert demands we fulfill
+                  it within a day. '/>
+    <ContactContainer  
+      detail = {<span className='detail'><span>Contact us and we will get back to you within 24 hours.</span><br/>
+        <span>Kampala Uganda</span><br/><span>+256 778 800 900</span><br/><span>company.gmail.com</span></span>}/> 
+  </div>
+  );
+}
+
+const Header = ()=>{
+  return(
+    <div className='header'>
+      <span className='header-title'>DeccanComputers</span>
+      <br/>
+      <span className="header-content">This is about taking it deep.</span>
+    </div>
+  );
+}
+
+const Card = (props) =>{
+  return(
+    <div className={props.className}>
+      <div className="small-div">
+        <i className={props.className}></i>
+        <img src={props.img} alt='' height = '100 px' width = '100 px'/>
+      </div>
+      <div className="big-div">
+        <span className="div-titlecard">
+          {props.title}
+        </span>
+        <br/>
+        <span>
+          {props.description}
+        </span>
+      </div>
+    </div>
+  );
+}
+
+const ContactContainer = (props) => {
+  var myStyle = {background : '#000000'}
+  return(
+      <div style={myStyle} className='contact-container'>
+        {/*demonstration of using inline css*/}    
+        <span className="div-title">Contact us</span>
+            <div className='contact-form'>
+                <div id='sect1'>
+                    {props.detail}
+                </div>           
+                <div id='sect2'>
+                    <input type="text" placeholder="email address" className="input-field"/>
+                    <textarea name="" id="" cols="50" rows="8" placeholder="comment"></textarea>
+                    <button className="contact-btn">Send</button>
+                </div>
+            </div>
+            <hr width="90%" color="#ffffff"></hr>
+            <p id="company">DeccanComputers</p>
+            <a href={pdf} download >About</a>
+        </div>
+  );
+
+}
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Body />,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
